ovh/manager

fix(container): allow account sidebar toggle on small screen
ref: MANAGER-9053
Signed-off-by: Chloe Renaud <chloe.renaud.ext@ovhcloud.com>

packages/components/ovh-shell/src/plugin/ux/components/sidebar.ts
@@ -3,15 +3,18 @@ import { UXComponent, IUxComponent } from './component';
 interface ISidebar extends IUxComponent {
   enableToggle: () => void;
   disableToggle: () => void;
+  setForceLargeScreenDisplay: (v: boolean) => void;
   isToggleAllowed: () => boolean;
 }
 
 export default class Sidebar extends UXComponent implements ISidebar {
   private allowToggle?: boolean;
+  private forceLargeScreenDisplay?: boolean;
 
   constructor() {
     super({ visible: false });
     this.allowToggle = true;
+    this.forceLargeScreenDisplay = false;
   }
 
   public isToggleAllowed(): boolean {
@@ -24,6 +27,14 @@ export default class Sidebar extends UXComponent implements ISidebar {
 
   public disableToggle(): void {
     this.allowToggle = false;
+  }
+
+  public setForceLargeScreenDisplay(v: boolean): void {
+    this.forceLargeScreenDisplay = v;
+  }
+
+  public isLargeScreenDisplayForced(): boolean {
+    return this.forceLargeScreenDisplay;
   }
 
   public onSidebarVisibilityChange(callback: CallableFunction): void {

packages/components/ovh-shell/src/plugin/ux/index.ts
@@ -10,6 +10,7 @@ export interface IUXPlugin {
   hideNotificationsSidebar(): void;
   enableAccountSidebarVisibilityToggle(): void;
   disableAccountSidebarVisibilityToggle(): void;
+  setForceAccountSiderBarDisplayOnLargeScreen(v: boolean): void;
   toggleNotificationsSidebarVisibility(): void;
   toggleAccountSidebarVisibility(): void;
   getUserIdCookie(): string;
@@ -40,6 +41,10 @@ export class UXPlugin implements IUXPlugin {
     return this.shellUX.isSidebarVisible('account');
   }
 
+  isAccountSidebarLargeScreenDisplayForced(): boolean {
+    return this.shellUX.isLargeScreenDisplayForced('account');
+  }
+
   showAccountSidebar(): void {
     return this.shellUX.showSidebar('account');
   }
@@ -54,6 +59,10 @@ export class UXPlugin implements IUXPlugin {
 
   disableAccountSidebarVisibilityToggle(): void {
     this.shellUX.disableSidebarToggle('account');
+  }
+
+  setForceAccountSiderBarDisplayOnLargeScreen(v: boolean): void {
+    this.shellUX.setForceSiderBarDisplayOnLargeScreen('account', v);
   }
 
   toggleAccountSidebarVisibility(): void {
@@ -80,6 +89,10 @@ export class UXPlugin implements IUXPlugin {
 
   toggleNotificationsSidebarVisibility(): void {
     this.shellUX.toggleSidebarVisibility('notifications');
+  }
+
+  onNotificationsSidebarVisibilityChange(callback: CallableFunction): void {
+    this.shellUX.onSidebarVisibilityChange('notifications', callback);
   }
 
   /* ----------- SSOAuthModal methods -----------*/

packages/components/ovh-shell/src/plugin/ux/ux.ts
@@ -64,6 +64,18 @@ export class ShellUX implements IShellUx {
     registeredSidebar?.disableToggle();
   }
 
+  isLargeScreenDisplayForced(sidebarName: string): boolean {
+    const registeredSidebar = this.sidebars[sidebarName];
+
+    return registeredSidebar?.isLargeScreenDisplayForced();
+  }
+
+  setForceSiderBarDisplayOnLargeScreen(sidebarName: string, v: boolean): void {
+    const registeredSidebar = this.sidebars[sidebarName];
+
+    registeredSidebar?.setForceLargeScreenDisplay(v);
+  }
+
   onSidebarVisibilityChange(
     sidebarName: string,
     callback: CallableFunction,

trnsnt/ovh-manager

packages/components/ovh-shell/src/plugin/ux/components/sidebar.ts
@@ -3,15 +3,18 @@ import { UXComponent, IUxComponent } from './component';
 interface ISidebar extends IUxComponent {
   enableToggle: () => void;
   disableToggle: () => void;
+  setForceLargeScreenDisplay: (isForced: boolean) => void;
   isToggleAllowed: () => boolean;
 }
 
 export default class Sidebar extends UXComponent implements ISidebar {
   private allowToggle?: boolean;
+  private forceLargeScreenDisplay?: boolean;
 
   constructor() {
     super({ visible: false });
     this.allowToggle = true;
+    this.forceLargeScreenDisplay = false;
   }
 
   public isToggleAllowed(): boolean {
@@ -24,6 +27,14 @@ export default class Sidebar extends UXComponent implements ISidebar {
 
   public disableToggle(): void {
     this.allowToggle = false;
+  }
+
+  public setForceLargeScreenDisplay(isForced: boolean): void {
+    this.forceLargeScreenDisplay = isForced;
+  }
+
+  public isLargeScreenDisplayForced(): boolean {
+    return this.forceLargeScreenDisplay;
   }
 
   public onSidebarVisibilityChange(callback: CallableFunction): void {

packages/components/ovh-shell/src/plugin/ux/index.ts
@@ -10,6 +10,7 @@ export interface IUXPlugin {
   hideNotificationsSidebar(): void;
   enableAccountSidebarVisibilityToggle(): void;
   disableAccountSidebarVisibilityToggle(): void;
+  setForceAccountSiderBarDisplayOnLargeScreen(isForced: boolean): void;
   toggleNotificationsSidebarVisibility(): void;
   toggleAccountSidebarVisibility(): void;
   getUserIdCookie(): string;
@@ -40,6 +41,10 @@ export class UXPlugin implements IUXPlugin {
     return this.shellUX.isSidebarVisible('account');
   }
 
+  isAccountSidebarLargeScreenDisplayForced(): boolean {
+    return this.shellUX.isLargeScreenDisplayForced('account');
+  }
+
   showAccountSidebar(): void {
     return this.shellUX.showSidebar('account');
   }
@@ -54,6 +59,10 @@ export class UXPlugin implements IUXPlugin {
 
   disableAccountSidebarVisibilityToggle(): void {
     this.shellUX.disableSidebarToggle('account');
+  }
+
+  setForceAccountSiderBarDisplayOnLargeScreen(isForced: boolean): void {
+    this.shellUX.setForceSiderBarDisplayOnLargeScreen('account', isForced);
   }
 
   toggleAccountSidebarVisibility(): void {
@@ -80,6 +89,10 @@ export class UXPlugin implements IUXPlugin {
 
   toggleNotificationsSidebarVisibility(): void {
     this.shellUX.toggleSidebarVisibility('notifications');
+  }
+
+  onNotificationsSidebarVisibilityChange(callback: CallableFunction): void {
+    this.shellUX.onSidebarVisibilityChange('notifications', callback);
   }
 
   /* ----------- SSOAuthModal methods -----------*/

packages/components/ovh-shell/src/plugin/ux/ux.ts
@@ -64,6 +64,18 @@ export class ShellUX implements IShellUx {
     registeredSidebar?.disableToggle();
   }
 
+  isLargeScreenDisplayForced(sidebarName: string): boolean {
+    const registeredSidebar = this.sidebars[sidebarName];
+
+    return registeredSidebar?.isLargeScreenDisplayForced();
+  }
+
+  setForceSiderBarDisplayOnLargeScreen(sidebarName: string, isForced: boolean): void {
+    const registeredSidebar = this.sidebars[sidebarName];
+
+    registeredSidebar?.setForceLargeScreenDisplay(isForced);
+  }
+
   onSidebarVisibilityChange(
     sidebarName: string,
     callback: CallableFunction,